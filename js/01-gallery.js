ff9fee6fd6b62d85d332011b741878e6d7eb9289
OksanaV2284 <110124340+OksanaV2284@users.noreply.github.com>
2022-12-20 20:39:04 +0200

    h/w 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
+import { galleryItems } from "./gallery-items.js";
+const onGallery = document.querySelector(".gallery");
 console.log(galleryItems);
+
+onGallery.insertAdjacentHTML(
+  "beforeend",
+  galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+      <a class="gallery__link" href='${original}'>
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source='${original}'
+          alt="${description}"
+        />
+      </a>
+    </div>`
+    )
+    .join("")
+);
+
+onGallery.addEventListener("click", onClickImg);
+function onClickImg(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${e.target.dataset.source}" width="800" height = "600" >`
+  );
+
+  instance.show();
+
+  onGallery.addEventListener("keydown", (e) => {
+    if (e.code === "Escape") {
+      instance.close();
+    }
+  });
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+console.log(SimpleLightbox);
 
 console.log(galleryItems);
+const onGallery = document.querySelector(".gallery");
+onGallery.insertAdjacentHTML(
+  "beforeend",
+  galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<li >
+      <a class="gallery__item" href='${original}'>
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source='${original}'
+          alt="${description}"
+        />
+      </a>
+    </li>`
+    )
+    .join("")
+);
+
+onGallery.addEventListener(
+  "click",
+  new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  })
+);
+// );
+// function onClickImg(e) {
+//   e.preventDefault();
+
+//   new SimpleLightbox(".gallery a", {
+//     captionsData: "alt",
+//     captionDelay: 250,
+//   });
+// }
